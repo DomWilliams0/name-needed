81561e4c40b164a7bed27230264c21e4aab915fe
Dom Williams <me@domwillia.ms>
2021-01-17 13:45:04 +0000

    Remove SDL dependency from build pipeline

--- .build/run-tests.sh
+++ .build/run-tests.sh
@@ -4,5 +4,8 @@ set -e
 export RUST_BACKTRACE=1
 export NN_LOG=debug
 
-cargo test --verbose --workspace
-cargo test --verbose -- --ignored
+# avoid SDL dependency by not running renderer tests
+FLAGS="--verbose --workspace --exclude engine --exclude main"
+
+cargo test $FLAGS
+cargo test $FLAGS -- --ignored
