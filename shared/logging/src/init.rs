bc0354bc562774b284a8729e0dbd444c23718e91
Dom Williams <me@domwillia.ms>
2021-04-09 16:25:29 +0100

    Add logging to file as well as terminal

--- shared/logging/src/init.rs
+++ shared/logging/src/init.rs
@@ -4,6 +4,7 @@ use std::fmt::{Display, Formatter};
 use slog::{Drain, Level};
 use slog_scope::GlobalLoggerGuard;
 use slog_term::ThreadSafeTimestampFn;
+use std::fs::OpenOptions;
 
 pub struct LoggerBuilder {
     level: Level,
@@ -14,6 +15,7 @@ pub struct Logger(Level, GlobalLoggerGuard);
 #[derive(Debug)]
 pub enum LogError {
     BadLevel(String),
+    Io(std::io::Error),
 }
 
 impl LoggerBuilder {
@@ -35,15 +37,51 @@ impl LoggerBuilder {
 
     // TODO configure to write to file as text
 
-    pub fn init(self, timestamp_fn: impl ThreadSafeTimestampFn) -> Result<Logger, LogError> {
-        let decorator = slog_term::TermDecorator::new()
-            .stderr()
-            .force_color()
-            .build();
-        let drain = slog_term::CompactFormat::new(decorator)
-            .use_custom_timestamp(timestamp_fn)
-            .build()
-            .fuse();
+    pub fn init(self, timestamp_fn: impl ThreadSafeTimestampFn + Copy) -> Result<Logger, LogError> {
+        let terminal_drain = {
+            let decorator = slog_term::TermDecorator::new()
+                .stderr()
+                .force_color()
+                .build();
+            slog_term::CompactFormat::new(decorator)
+                .use_custom_timestamp(timestamp_fn)
+                .build()
+                .fuse()
+        };
+
+        let drain = {
+            #[cfg(feature = "to-file")]
+            {
+                let file_drain = {
+                    let log_file = std::env::temp_dir().join("nn-logs");
+
+                    if log_file.is_file() {
+                        // try to backup
+                        let mut bak = log_file.clone();
+                        bak.set_extension("bak");
+                        let _ = std::fs::rename(&log_file, &bak);
+                    }
+
+                    let file = OpenOptions::new()
+                        .create(true)
+                        .truncate(true)
+                        .write(true)
+                        .open(log_file)
+                        .map_err(LogError::Io)?;
+                    let decorator = slog_term::PlainDecorator::new(file);
+                    slog_term::FullFormat::new(decorator)
+                        .use_custom_timestamp(timestamp_fn)
+                        .build()
+                        .fuse()
+                };
+
+                slog::Duplicate::new(terminal_drain, file_drain)
+            }
+
+            #[cfg(not(feature = "to-file"))]
+            terminal_drain
+        };
+
         let drain = drain.filter_level(self.level).fuse();
         let drain = slog_async::Async::new(drain)
             .thread_name("logging".to_owned())
@@ -73,6 +111,7 @@ impl Display for LogError {
     fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
         match self {
             LogError::BadLevel(s) => write!(f, "Invalid level {:?}", s),
+            LogError::Io(e) => write!(f, "Io error opening log file: {}", e),
         }
     }
 }
